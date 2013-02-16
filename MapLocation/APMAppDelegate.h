8bd105831225bd167a40ff919995dbbb4b85a7b5
Ana Mei <peishi@seas.upenn.edu>
2013-02-16 01:22:43 -0500

    Initial Commit

---
+++ MapLocation/APMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APMAppDelegate.h
+//  MapLocation
+//
+//  Created by Ana Mei on 2/16/13.
+//  Copyright (c) 2013 Ana Mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapLocation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapLocation
+//
+//  Created by Ana Mei on 2/16/13.
+//  Copyright (c) 2013 Ana Mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APMAppDelegate class]));
+    }
+}
